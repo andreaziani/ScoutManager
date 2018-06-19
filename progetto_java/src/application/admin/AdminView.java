646f23d306adf7bcba46f436c3b5a27a02db2ffa
andreaziani <andrea.ziani@studio.unibo.it>
2018-06-19 22:16:25 +0200

    completed Admin's view

--- progetto_java/src/application/admin/AdminView.java
+++ progetto_java/src/application/admin/AdminView.java
@@ -1,26 +1,35 @@
 package application.admin;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Toolkit;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.border.Border;
 
 public class AdminView extends JFrame {
-	private int height = Toolkit.getDefaultToolkit().getScreenSize().height * 5/6;
-	private int width = Toolkit.getDefaultToolkit().getScreenSize().width * 5/6;
 	/**
 	 * Automatically generated.
 	 */
 	private static final long serialVersionUID = 8177523068486028650L;
-	
+	private int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+	private int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+	private JTextArea view = new JTextArea(100, 400);
+	private Border border = BorderFactory.createLineBorder(Color.BLACK);
 	public AdminView() {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 		InsertPanel insertPanel = new InsertPanel();
 		AdminViewOperationPanel op = new AdminViewOperationPanel();
+		AdminModifyOperation mod = new AdminModifyOperation();
+		panel.add(mod, BorderLayout.WEST);
 		panel.add(op, BorderLayout.SOUTH);
 		this.setTitle("Admin");
+		this.view.setBorder(this.border);
+		panel.add(this.view, BorderLayout.CENTER);
 		panel.add(insertPanel, BorderLayout.NORTH);
 		this.add(panel);
 		this.setSize(width, height);
