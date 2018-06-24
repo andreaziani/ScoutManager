7678e01ecc261ec9cf3d1709347eae049f6ecf65
andreaziani <andrea.ziani@studio.unibo.it>
2018-06-24 20:50:23 +0200

    implemented query

--- progetto_java/src/model/EventoNazionale.java
+++ progetto_java/src/model/EventoNazionale.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 public class EventoNazionale{
@@ -22,7 +24,27 @@ public class EventoNazionale{
 		this.località = località;
 		this.descrizione = descrizione;
 	}
-	
+	public String iscrittiAdEvento(String codiceEvento) {
+		PreparedStatement st;
+		String row = "CodIscritto \t Nome \t Cognome \t dataNascita \t NumeroTelefono \n";
+		try {
+			st = con.getMsSQLConnection().prepareStatement(
+					"select I.codiceIscritto, I.nome, I.cognome, I.dataNascita, I.numeroTelefono from REGISTRAZIONE_E_N R join ISCRITTO I on (R.codiceIscritto = I.codiceIscritto) where R.codiceEvento = ? ");
+			st.setString(1, codiceEvento);
+			ResultSet rs = st.executeQuery();
+			ResultSetMetaData rsm = st.getMetaData();
+			while(rs.next()) {
+				for(int i = 1; i <= rsm.getColumnCount(); i++) {
+					row += rs.getString(i);
+				}
+				row += "\n";
+			}
+		} catch (SQLException e) {
+			return row;
+		}
+		return row;
+		
+	}
 	public int execQuery() {
 	    int result;
 	    try {
--- progetto_java/src/view/responsabilenazionale/ViewEventdByDate.java
+++ progetto_java/src/view/responsabilenazionale/ViewEventdByDate.java
@@ -11,6 +11,7 @@ import java.util.Properties;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EtchedBorder;
 
@@ -22,74 +23,90 @@ import model.ResponsabileEventoNazionale;
 import model.ResponsabilitàEventoNazionale;
 import view.DateLabelFormatter;
 
-public class ViewEventdByDate extends JPanel{
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 8835226786280861286L;
-    
-    
-    private GridBagConstraints gbc= new GridBagConstraints();
-    private JLabel title = new JLabel("Visualizza eventi per data");
-    private JLabel dataInizio = new JLabel("Data di inizio:");
-    private Properties p;
-    private JDatePickerImpl datePicker;
-    private JButton btn = new JButton("VISUALIZZA");
-    
-    
-    public ViewEventdByDate(ResponsabileEventoNazionale res, ConsoleArea log) {
-        this.setBorder(new EtchedBorder(EtchedBorder.LOWERED));
-        this.setLayout(new GridBagLayout());
-        p = new Properties();
-        p.put("text.today", "Today");
-        p.put("text.month", "Month");
-        p.put("text.year", "Year");
-        setTitle();
-        this.build();
-        this.btn.addActionListener(e->{
-            ResponsabilitàEventoNazionale r = new ResponsabilitàEventoNazionale(null, res.getCodiceResponsabile());
-            String msg = r.viewEvbyDateQuery((Date) datePicker.getModel().getValue());
-            log.insert(msg);
-        });
-    }
-    
-    private void setTitle() {
-        GridBagConstraints gbcMain = new GridBagConstraints();
-        gbcMain.fill= GridBagConstraints.NONE;
-        gbcMain.gridx = 0;
-        gbcMain.gridy = 0;
-        
-        gbcMain.anchor = GridBagConstraints.CENTER;
-        gbcMain.insets = new Insets(2, 0, 3, 0);
-        gbcMain.insets = new Insets(4, 0, 4, 0);
-        title.setForeground(Color.RED);
-        title.setHorizontalAlignment(SwingConstants.CENTER);
-        title.setAlignmentX(Component.CENTER_ALIGNMENT);
-        gbcMain.gridwidth = GridBagConstraints.REMAINDER;
-        this.add(title, gbcMain);
-    }
-    
-    private void build() {
-        gbc.insets = new Insets(3, 3, 3, 3);
-        gbc.gridy = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.weightx = 1.0;
-        
-        datePicker = new JDatePickerImpl(new JDatePanelImpl(new UtilDateModel(), p), new DateLabelFormatter());
-        
-        add(dataInizio, gbc);
-        
-        gbc.gridy = 2;
-        gbc.gridx = 0;
-        
-        add(datePicker, gbc);
-        
-        gbc.gridy = 3;
-        gbc.gridx = 0;
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        gbc.gridheight = GridBagConstraints.REMAINDER;
-        gbc.fill = GridBagConstraints.NONE;
-        add(btn, gbc);
-    }
-    
+public class ViewEventdByDate extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8835226786280861286L;
+
+	private GridBagConstraints gbc = new GridBagConstraints();
+	private JLabel title = new JLabel("Visualizza eventi per data");
+	private JLabel dataInizio = new JLabel("Data di inizio:");
+	private JLabel title2 = new JLabel("Visualizza iscritti ad evento");
+	private JLabel codice = new JLabel("Codice evento: ");
+	private JTextField codiceEvento = new JTextField(16);
+	private Properties p;
+	private JDatePickerImpl datePicker;
+	private JButton btn = new JButton("VISUALIZZA");
+
+	public ViewEventdByDate(ResponsabileEventoNazionale res, ConsoleArea log) {
+		this.setBorder(new EtchedBorder(EtchedBorder.LOWERED));
+		this.setLayout(new GridBagLayout());
+		p = new Properties();
+		p.put("text.today", "Today");
+		p.put("text.month", "Month");
+		p.put("text.year", "Year");
+		setTitle();
+		this.build();
+		this.btn.addActionListener(e -> {
+			ResponsabilitàEventoNazionale r = new ResponsabilitàEventoNazionale(null, res.getCodiceResponsabile());
+			String msg = r.viewEvbyDateQuery((Date) datePicker.getModel().getValue());
+			log.insert(msg);
+		});
+	}
+
+	private void setTitle() {
+		GridBagConstraints gbcMain = new GridBagConstraints();
+		gbcMain.fill = GridBagConstraints.NONE;
+		gbcMain.gridx = 0;
+		gbcMain.gridy = 0;
+
+		gbcMain.anchor = GridBagConstraints.CENTER;
+		gbcMain.insets = new Insets(2, 0, 3, 0);
+		gbcMain.insets = new Insets(4, 0, 4, 0);
+		title.setForeground(Color.RED);
+		title2.setForeground(Color.RED);
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setAlignmentX(Component.CENTER_ALIGNMENT);
+		gbcMain.gridwidth = GridBagConstraints.REMAINDER;
+		this.add(title, gbcMain);
+	}
+
+	private void build() {
+		gbc.insets = new Insets(3, 3, 3, 3);
+		gbc.gridy = 1;
+		gbc.gridx = 0;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.weightx = 1.0;
+
+		datePicker = new JDatePickerImpl(new JDatePanelImpl(new UtilDateModel(), p), new DateLabelFormatter());
+
+		add(dataInizio, gbc);
+
+		gbc.gridy = 2;
+		gbc.gridx = 0;
+
+		add(datePicker, gbc);
+
+		gbc.gridy = 3;
+		gbc.gridx = 0;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.gridheight = GridBagConstraints.REMAINDER;
+		gbc.fill = GridBagConstraints.NONE;
+		add(btn, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+
+		add(title2, gbc);
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+		add(codice, gbc);
+		gbc.gridy = 6;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.gridheight = GridBagConstraints.REMAINDER;
+		add(codiceEvento, gbc);
+	}
+
 }
